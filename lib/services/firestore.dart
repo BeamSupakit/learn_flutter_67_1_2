252b9eee8f2fc10e54f15d7d21c958b1f7957499
Supakit <beam0820234866@gmail.com>
2025-09-06 15:16:25 +0700

    Step 5: Make a firestore service

---
+++ lib/services/firestore.dart
@@ -0,0 +1,43 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirestoreService {
+  final CollectionReference persons = FirebaseFirestore.instance.collection(
+    'persons',
+  );
+
+  Future<void> addPerson(String personName, String personEmail, int personAge) {
+    return persons.add({
+      'personName': personName,
+      'personEmail': personEmail,
+      'personAge': personAge,
+      'Timestamp.now': Timestamp.now(),
+    });
+  }
+
+  Stream<QuerySnapshot> getPersons() {
+    return persons.orderBy('tqimestamp', descending: true).snapshots();
+  }
+
+  Future<Map<String, dynamic>?> getPersonById(String personId) async {
+    final doc = await persons.doc(personId).get();
+    return (doc.data() as Map<String, dynamic>?) ?? {};
+  }
+
+  Future<void> updatePerson(
+    String personId,
+    String personName,
+    String personEmail,
+    int personAge,
+  ) {
+    return persons.doc(personId).update({
+      'personName': personName,
+      'personEmail': personEmail,
+      'personAge': personAge,
+      'Timestamp.now': Timestamp.now(),
+    });
+  }
+
+  Future<void> deletePerson(String personId) {
+    return persons.doc(personId).delete();
+  }
+}
